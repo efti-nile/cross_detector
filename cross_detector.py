c27597958b4b5d82b32cf8decebaf85fb0ed85b9
efti-nile <efti.nile@gmail.com>
2020-04-09 15:03:30 +0400

    Implemented traslating IoU

--- cross_detector.py
+++ cross_detector.py
@@ -33,15 +33,16 @@ class CrossDetector:
                 _, y1, _, y2 = box
                 lower_y = max(y1, y2)
                 if self.gate_y_coor < lower_y < self.gate_y_coor + self.gate_width:
-                    self.track_boxes.append(TrackingBox(mask, box, img))
+                    self.track_boxes.append(TrackingBox(mask, box, img, date))
         return out
 
 
 class TrackingBox:
 
-    IOU_THRESH = 0.9
+    IOU_THRESH = 0.5
 
     def __init__(self, init_mask, init_box, init_img, init_date):
+        assert isinstance(init_box, np.ndarray) and init_box.ndim == 1 and init_box.size == 4
         self.masks = [init_mask]
         self.boxes = [init_box]
         self.imgs = [init_img]
@@ -50,69 +51,44 @@ class TrackingBox:
         self.lower_y = max(init_box[1], init_box[3])  # the lower point has maximum y coordinate
 
     def IoU_with_mask(self, mask, box):
+        assert isinstance(box, np.ndarray) and box.ndim == 1 and box.size == 4
         if self.is_intersection(box):
-            sbox = self.boxes[-1]  # self box
-            min_x = min(box[0], sbox[0])
-            min_y = min(box[1], sbox[1])
-            smask_with_padding = np.full((h_px, w_px), False)
-            mask_with_padding = np.full((h_px, w_px), False)
-            # sbox = self.boxes[-1]  # self box
-            # smask = self.mask[-1].copy()  # self mask
-            # mask = mask.copy()
-            #
-            # # Expansion size
-            # w_px = np.round(max(box[2], sbox[2]) - min(box[0], sbox[0])).astype("int")
-            # h_px = np.round(max(box[3], sbox[3]) - min(box[1], sbox[1])).astype("int")
-            #
-            # smask_with_padding = np.full((h_px, w_px), False)
-            # mask_with_padding = np.full((h_px, w_px), False)
-            # if max(box[1], box[3]) > max(sbox[1], sbox[3]):
-            #     if max(box[0], box[2]) > max(sbox[0], sbox[2]):
-            #         #   ----------
-            #         #  |smask     |
-            #         #  |          |
-            #         #  |      mask|
-            #         #   ----------
-            #         mask_with_padding[-mask.shape[0]:, -mask.shape[1]:] =
-            #         smask_with_padding[] =
-            #     else:  # sbox to the right of the box
-            #         #   ----------
-            #         #  |          |
-            #         #  |          |
-            #         #  |          |
-            #         #   ----------
-            #         pass
-            # else:  # sbox lower than box
-            #     if max(box[0], box[2]) > max(sbox[0], sbox[2]):  # box to the right of the sbox
-            #         #   ----------
-            #         #  |          |
-            #         #  |          |
-            #         #  |          |
-            #         #   ----------
-            #         pass
-            #     else:  # sbox to the right of the box
-            #         #   ----------
-            #         #  |          |
-            #         #  |          |
-            #         #  |          |
-            #         #   ----------
-            #         pass
+            sbox = self.boxes[-1].copy()  # self box
+            smask = self.masks[-1]  # self mask
+            cbox = box.copy()  # copy of box
 
-            return (self.masks[-1] & mask).sum() / (self.masks[-1] | mask).sum()
+            # Translate detections to the origin to reduce an amount of padding
+            min_x = min(cbox[0], sbox[0])
+            min_y = min(cbox[1], sbox[1])
+            offset = np.array([min_x, min_y, min_x, min_y])
+            cbox = np.round(cbox - offset).astype(np.int)
+            sbox = np.round(sbox - offset).astype(np.int)
+
+            # Padding to common size
+            total_width_px = max(cbox[2], sbox[2]) + 1  # +1 just in case
+            total_height_px = max(cbox[3], sbox[3]) + 1  # +1 just in case
+            smask_with_padding = np.full((total_height_px, total_width_px), False)
+            mask_with_padding = np.full((total_height_px, total_width_px), False)
+            smask_with_padding[sbox[1]:(sbox[1] + smask.shape[0]), sbox[0]:(sbox[0] + smask.shape[1])] = smask
+            mask_with_padding[cbox[1]:(cbox[1] + mask.shape[0]), cbox[0]:(cbox[0] + mask.shape[1])] = mask
+
+            # IoU
+            IoU = (smask_with_padding & mask_with_padding).sum() / (smask_with_padding | mask_with_padding).sum()
+            print(IoU)
+            return (smask_with_padding & mask_with_padding).sum() / (smask_with_padding | mask_with_padding).sum()
         else:
             return 0
 
     def is_intersection(self, box):
         x1, y1, x2, y2 = box
         x3, y3, x4, y4 = self.boxes[-1]
-        assert x2 > x1 and x4 > x3
         x_cross = TrackingBox.intersection_1d(x1, x2, x3, x4)
-        assert y2 > y1 and y4 > y3
         y_cross = TrackingBox.intersection_1d(y1, y2, y3, y4)
         return x_cross * y_cross > 0
 
     @staticmethod
     def intersection_1d(start1, end1, start2, end2):
+        assert start1 < end1 and start2 < end2
         start = max(start1, start2)
         end = min(end1, end2)
         if start < end:
@@ -121,30 +97,27 @@ class TrackingBox:
             return 0
 
     def update(self, masks_in_frame, boxes_in_frame, imgs_in_frame, date):
-        for mask, box, img in zip(masks_in_frame, boxes_in_frame, imgs_in_frame):
-            if self.IoU_with_mask(mask, box) > self.IOU_THRESH:
+        assert len(masks_in_frame) == len(boxes_in_frame) == len(imgs_in_frame)
+        for i in range(len(masks_in_frame)):
+            if self.IoU_with_mask(masks_in_frame[i], boxes_in_frame[i]) > self.IOU_THRESH:
                 # Append a new detection to the track
-                self.masks.append(mask)
-                self.boxes.append(box)
-                self.imgs.append(img)
+                self.masks.append(masks_in_frame.pop(i))
+                self.boxes.append(boxes_in_frame.pop(i))
+                self.imgs.append(imgs_in_frame.pop(i))
                 self.dates.append(date)
                 self.track_len += 1
 
                 # Update lower track border `lower_y`
-                y = max(box[1], box[3])
+                y = max(self.boxes[-1][1], self.boxes[-1][3])
                 if y > self.lower_y:
                     self.lower_y = y
-
-                # Remove used detection from list of all ones
-                masks_in_frame.remove(mask)
-                boxes_in_frame.remove(box)
-                imgs_in_frame.remove(img)
                 break
         return masks_in_frame, boxes_in_frame, imgs_in_frame
 
 
 if __name__ == '__main__':
     cd = CrossDetector('data/in_out/in_video.mp4', 650)
+    tbs = []
     for _ in range(150):
-        cd.process_next_frame()
+        tbs += cd.process_next_frame()
     pass
